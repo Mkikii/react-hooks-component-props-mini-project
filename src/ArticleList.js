d8f338fa409a5fcbab803920814461aca6316b2b
Mkikii <maureenwkarimi@gmail.com>
2025-07-28 22:22:05 +0300

    All tests passing

---
+++ src/ArticleList.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Article from "./Article";
+
+const ArticleList = ({ posts }) => {
+  return (
+    <main>
+      {posts.map((post) => (
+        <Article
+          key={post.id}
+          title={post.title}
+          preview={post.preview}
+          minutes={post.minutes}
+        />
+      ))}
+    </main>
+  );
+};
+
+export default ArticleList;
